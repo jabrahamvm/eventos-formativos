f7cb1bb6448ec6e69b2217dba26ab10a428c539f
jabrahamvm <abrahamvillalba00@gmail.com>
2022-11-28 18:14:20 -0700

    Baseline for a fullstack component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/*import React from 'react';
+
 
 const Home = () => {
     return (
@@ -7,3 +8,59 @@ const Home = () => {
 }
 
 export default Home
+*/
+import axios from 'axios';
+import { useState, useEffect} from 'react';
+//import {link} from 'react-router-dom';
+
+const URL = 'http://localhost:8000/api/solicitudes/';
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(()=>{
+        getBlogs()
+    },[])
+
+    const getBlogs = async () => {
+        const res = await axios.get(URL)
+        setBlogs(res.data)
+    }
+
+    const deleteBlog = async (id) => {
+        await axios.delete(`${URL}${id}/`)
+        getBlogs()
+    }
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <table className='table'>
+                        <thead className='table-primary'>
+                            <tr>
+                                <th>Title</th>
+                                <th>Content</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {blogs.map((blog) => (
+                                <tr key={blog.id}>
+                                    <td>{blog.titulo}</td>
+                                    <td>{blog.descripcion}</td>
+                                    <td>{blog.estado}</td>
+                                    <td>
+                                        {/*<link to={`/edit/${blog.id}`} className="btn btn-info">Editar</link>*/}
+                                        <button onClick={() => deleteBlog(blog.id)} className='btn btn-danger'>Eliminar</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
