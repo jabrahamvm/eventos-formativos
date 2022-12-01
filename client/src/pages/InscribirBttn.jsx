cd9815a0527fc41bd5cd7cba4f3bdb882a418a10
Astralpuppy <rodrigonev98@gmail.com>
2022-12-01 01:03:03 -0700

    Desactivar boton despues de inscribirse.

--- client/src/pages/InscribirBttn.jsx
+++ client/src/pages/InscribirBttn.jsx
@@ -7,18 +7,40 @@ const URL = 'http://localhost:8000/api/usuario/inscribir/';
 
 const BotonInscribir = ({idUsuario, idEvento}) => {
 
+    const [Eventos, setEventos] = useState([])
+
+    useEffect(() => {
+        getEventos()
+        
+    },[])
+
+    const getEventos = async () => {
+        const res = await axios.get(`http://localhost:8000/api/usuario/${idUsuario}/eventos/`)
+        console.log(res.data)
+        setEventos(res.data.Eventos)
+    }
+
+    const Inscritos = Eventos.map((evento) => evento.id)
     const navigate = useNavigate()
     const inscripcion = async (e) => {
         e.preventDefault()
         await axios.post(URL,{idUsuario:idUsuario,idEvento:idEvento})
         navigate('/EventosInscritos/5')
     }
-
-    return (
-        <form onSubmit={inscripcion}>
-            <button type='submit' className='btn btn-success'>Inscribirme</button>
-        </form>
-    )
+    
+    if (Inscritos.includes(idEvento*1)){
+        return (
+            <form onSubmit={inscripcion}>
+                <button type='submit' className='btn btn-success' disabled>Inscribirme</button>
+            </form>
+        )
+    } else {
+        return (
+            <form onSubmit={inscripcion}>
+                <button type='submit' className='btn btn-success'>Inscribirme</button>
+            </form>
+        )
+    }
 }
 
 export default BotonInscribir;
